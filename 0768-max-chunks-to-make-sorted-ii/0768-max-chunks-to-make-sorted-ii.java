71962d9980493257e55e95dae82f623bfd31f4aa
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2023-10-19 10:11:36 +0530

    Time: 3 ms (16.73%), Space: 43.1 MB (48.39%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
@@ -1,5 +1,10 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
+        //return Approach1(arr);
+        return Approach2(arr);
+    }
+    public int Approach1(int[] arr)
+    {
         int[] leftMax = new int[arr.length];
         int[] rightMin = new int[arr.length];
         leftMax[0]=arr[0];
@@ -19,5 +24,20 @@ class Solution {
                 ans++;
         }
         return ans;
+    }
+    public int Approach2(int[] arr)
+    {
+        Stack<Integer> st = new Stack<>();
+        int large=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            while(!st.isEmpty() && st.peek()>arr[i])
+            {
+                st.pop();
+            }
+            large = Math.max(large,arr[i]);
+            st.push(large);
+        }
+        return st.size();
     }
 }
