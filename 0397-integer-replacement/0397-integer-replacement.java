3e13b52b11530439396721a0988e2a8e5000249e
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2023-10-30 21:47:25 +0530

    Time: 4 ms (28.75%), Space: 38.7 MB (93.04%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -1,6 +1,6 @@
 class Solution {
     public int integerReplacement(int n) {
-        return miniOperation(n);
+       return miniOperation(n);
     }
     public int miniOperation(int n)
     {
@@ -15,6 +15,23 @@ class Solution {
         else
         {
             return 1+(Math.min(miniOperation(n-1),miniOperation(n+1)));
+        }
+    }
+    public int memoization(int n, int[]dp)
+    {
+        if(n == Integer.MAX_VALUE)
+            return 32;
+        if(n==1)
+            return 0;
+        if(dp[n]!=0)
+            return dp[n];
+        if(n%2==0)
+        {
+            return dp[n] = 1+memoization(n/2,dp);
+        }
+        else
+        {
+            return dp[n] = 1+(Math.min(memoization(n-1,dp),memoization(n+1,dp)));
         }
     }
 }
