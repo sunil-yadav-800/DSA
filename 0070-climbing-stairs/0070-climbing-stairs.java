5d559e4f7d729d22b853768b6637802d39a438a7
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2023-11-12 14:43:06 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (54.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,8 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
+        /*
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
         return Memoization(n,dp);
+        */
+        return Tabulation(n);
     }
     private int Memoization(int n, int[] dp)
     {
@@ -18,5 +21,21 @@ class Solution {
          step2 = Memoization(n-2,dp);
         }
         return dp[n] = step1+step2;
+    }
+    private int Tabulation(int n)
+    {
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            int step1 = dp[i-1];
+            int step2 = 0;
+            if(i>1)
+            {
+                step2 = dp[i-2];
+            }
+            dp[i]=step1+step2;
+        }
+        return dp[n];
     }
 }
