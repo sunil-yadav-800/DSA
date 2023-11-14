804342f7092d54db9cdc32d130cbcb3e0f733966
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2023-11-14 13:31:55 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (13.09%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,8 +1,11 @@
 class Solution {
     public int tribonacci(int n) {
+        /*
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
         return Memo(n,dp);
+        */
+        return Table(n);
     }
     private int Memo(int n,int[] dp)
     {
@@ -15,5 +18,24 @@ class Solution {
         if(dp[n]!=-1)
             return dp[n];
         return dp[n] = Memo(n-3,dp)+Memo(n-2,dp)+Memo(n-1,dp);
+    }
+    private int Table(int n)
+    {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 1;
+        int[] dp = new int[n+1];
+        //fill base case
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
+        }
+        return dp[n];
     }
 }
