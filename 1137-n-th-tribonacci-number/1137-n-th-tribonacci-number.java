971084c203487f64068303e09103e358bd0678a7
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2023-11-14 13:37:41 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (24.04%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -5,7 +5,10 @@ class Solution {
         Arrays.fill(dp,-1);
         return Memo(n,dp);
         */
-        return Table(n);
+        
+        //return Table(n);
+        
+        return spaceOptimize(n);
     }
     private int Memo(int n,int[] dp)
     {
@@ -37,5 +40,23 @@ class Solution {
             dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
         }
         return dp[n];
+    }
+    private int spaceOptimize(int n)
+    {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 1;
+        int prev3=0,prev2=1,prev1=1;
+        for(int i=3;i<=n;i++)
+        {
+            int curr = prev3+prev2+prev1;
+            prev3=prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
     }
 }
