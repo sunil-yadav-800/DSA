54f3a9159ca9d3d79f6cb2afa4fd4aef30a43a80
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2023-11-15 11:23:18 +0530

    Time: 4 ms (47.06%), Space: 44.4 MB (15.83%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
+        /*
         int m = triangle.size();
         int[][] dp = new int[m][m];
         for(int i=0;i<m;i++)
@@ -10,10 +11,12 @@ class Solution {
             }
         }
         return Solve1(triangle,0,0,dp);
+        */
+        return Solve2(triangle);
     }
     private int Solve1(List<List<Integer>> triangle, int row, int col, int[][] dp)
     {
-        //base 
+        //base case
         if(row == triangle.size()-1)
         {
             return triangle.get(row).get(col);
@@ -26,5 +29,25 @@ class Solution {
         int first = triangle.get(row).get(col) + Solve1(triangle, row+1, col, dp);
         int second = triangle.get(row).get(col) + Solve1(triangle, row+1, col+1, dp);
         return dp[row][col] = Math.min(first,second);
+    }
+    private int Solve2(List<List<Integer>> triangle)
+    {
+        int m = triangle.size();
+        int[][] dp = new int[m][m];
+        //base case
+        for(int col=0; col<m; col++)
+        {
+            dp[m-1][col]=triangle.get(m-1).get(col);
+        }
+        for(int row=m-2;row>=0;row--)
+        {
+            for(int col=row;col>=0;col--)
+            {
+                int first = triangle.get(row).get(col) + dp[row+1][col];
+                int second = triangle.get(row).get(col) + dp[row+1][col+1];
+                dp[row][col]=Math.min(first,second);
+            }
+        }
+        return dp[0][0];
     }
 }
