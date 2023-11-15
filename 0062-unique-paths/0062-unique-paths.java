be268245efd8607403d56102b1c97c2359c94cd5
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2023-11-15 12:54:03 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (18.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,7 +1,19 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        return Approach2(m,n);
+       // return Approach2(m,n);
+        //return Approach1(m,n);
+        
+        return Table(m,n);
+    }
+    public static int Approach1(int m, int n)
+    {
+        long ans=1;
+        for(int i=1;i<m;i++)
+        {
+            ans=ans*(n-1+i)/i;
+        }
+        return (int)ans;
     }
     public static int Approach2(int m, int n)
     {
@@ -32,5 +44,27 @@ class Solution {
         int right=unique(m,n,row,col+1,dp);
         int down=unique(m,n,row+1,col,dp);
         return dp[row][col]=right+down;
+    }
+    private static int Table(int m, int n)
+    {
+        int[][]dp=new int[m+1][n+1];
+        //base case
+        dp[m-1][n-1]=1;
+        //2nd base case handled already
+        
+        for(int row=m-1;row>=0;row--)
+        {
+            for(int col=n-1;col>=0;col--)
+            {
+                if(row == m-1 && col== n-1)
+                {
+                    continue;
+                }
+                int right=dp[row][col+1];
+                int down=dp[row+1][col];
+                dp[row][col]=right+down;
+            }
+        }
+        return dp[0][0];
     }
 }
