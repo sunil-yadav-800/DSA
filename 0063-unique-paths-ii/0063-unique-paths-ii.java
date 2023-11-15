d416f9a5295a838ce6bfbb152fa50e850fed9fe5
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2023-11-15 13:26:51 +0530

    Time: 1 ms (23.64%), Space: 40.4 MB (69.89%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -4,7 +4,7 @@ class Solution {
         int n = obstacleGrid[0].length-1;
         if(obstacleGrid[m][n]==1)
             return 0;
-        
+       /* 
         int[][] dp = new int[m+1][n+1];
         for(int i=0;i<m+1;i++)
         {
@@ -14,6 +14,8 @@ class Solution {
             }
         }
         return Solve1(obstacleGrid,m,n,dp);
+        */
+        return Solve2(obstacleGrid);
     }
     private int Solve1(int[][] grid, int row, int col, int[][] dp)
     {
@@ -34,5 +36,33 @@ class Solution {
             left = Solve1(grid,row,col-1,dp);
         }
         return dp[row][col] = up+left;
+    }
+    private int Solve2(int[][] grid)
+    {
+        int[][] dp = new int[grid.length][grid[0].length];
+        //base case
+        dp[0][0]=1;
+        for(int row=0;row<grid.length;row++)
+        {
+            for(int col=0;col<grid[0].length;col++)
+            {
+                if(row==0 && col==0)
+                    continue;
+                
+                int up = 0;
+                if(row-1>=0 && grid[row-1][col]!=1)
+                {
+                    up = dp[row-1][col];
+                }
+                int left = 0;
+                if(col-1>=0 && grid[row][col-1]!=1)
+                {
+                    left = dp[row][col-1];
+                }
+                
+               dp[row][col] = up+left;
+            }
+        }
+        return dp[grid.length-1][grid[0].length-1];
     }
 }
