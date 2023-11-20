d146d4acdfa4ce315b0cdbe26602efa15885aa54
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2023-11-20 19:15:09 +0530

    Time: 6 ms (52.89%), Space: 46.1 MB (16.14%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public int change(int amount, int[] coins) {
+        /*
         int[][] dp = new int[coins.length][amount+1];
         for(int i=0;i<dp.length;i++)
         {
@@ -9,6 +10,8 @@ class Solution {
             }
         }
         return solve(coins,coins.length-1,amount,dp);
+        */
+        return solve2(coins,amount);
     }
     private int solve(int[] coins, int idx, int amount, int[][] dp)
     {
@@ -27,5 +30,28 @@ class Solution {
         }
         int notTake = solve(coins,idx-1,amount,dp);
         return dp[idx][amount] = take+notTake;
+    }
+    private int solve2(int[] coins, int target)
+    {
+        int[][] dp = new int[coins.length][target+1];
+        //base case
+        for(int amount=0;amount<=target;amount++)
+        {
+            dp[0][amount]=amount%coins[0]==0?1:0;
+        }
+        for(int idx=1;idx<coins.length;idx++)
+        {
+            for(int amount=0;amount<=target;amount++)
+            {
+                int take = 0;
+                if(amount-coins[idx]>=0)
+                {
+                    take = dp[idx][amount-coins[idx]];
+                }
+                int notTake = dp[idx-1][amount];
+               dp[idx][amount] = take+notTake;
+            }
+        }
+        return dp[coins.length-1][target];
     }
 }
