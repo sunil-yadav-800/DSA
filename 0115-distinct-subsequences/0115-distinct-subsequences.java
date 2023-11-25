bc49a536e98bb8d98d11cda984d2d59fb67ecf97
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2023-11-25 18:51:29 +0530

    Time: 14 ms (70.13%), Space: 46.7 MB (66.68%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,5 +1,6 @@
 class Solution {
     public int numDistinct(String s, String t) {
+        /*
         int[][] dp = new int[s.length()+1][t.length()+1];
         for(int i=0;i<dp.length;i++)
         {
@@ -9,6 +10,8 @@ class Solution {
             }
         }
         return solve(s,t,0,0,dp);
+        */
+        return solve2(s,t);
     }
     private int solve(String s, String t, int i, int j, int[][] dp)
     {
@@ -34,5 +37,38 @@ class Solution {
         {
             return dp[i][j] = solve(s,t,i+1,j,dp);
         }
+    }
+    private int solve2(String s, String t)
+    {
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        //base case
+        //first base case should be up otherwise it will set last row,lastcol as 0
+        for(int i=0;i<dp[0].length;i++)
+        {
+            dp[s.length()][i]=0;
+        }
+        for(int i=0;i<dp.length;i++)
+        {
+            dp[i][t.length()]=1;
+        }
+        
+        
+        for(int i=dp.length-2;i>=0;i--)
+        {
+            for(int j=dp[0].length-2;j>=0;j--)
+            {
+                if(s.charAt(i) == t.charAt(j))
+                {
+                    int take = dp[i+1][j+1];
+                    int notTake = dp[i+1][j];
+                    dp[i][j] =  take+notTake;
+                }
+                else
+                {
+                     dp[i][j] = dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
