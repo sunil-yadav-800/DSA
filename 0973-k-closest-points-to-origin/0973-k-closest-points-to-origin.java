121856422f035db5463fa88d10d2184d7347e4e1
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2024-01-21 21:19:05 +0530

    Time: 22 ms (82.66%), Space: 48.1 MB (84.82%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,45 +1,17 @@
-class Axis{
-    int x;
-    int y;
-    public Axis(int x, int y)
-    {
-        this.x=x;
-        this.y=y;
-    }
-}
-class sortByDistance implements Comparator<Axis>{
-    public int compare(Axis a, Axis b)
-    {
-        double sq1 = Math.sqrt(Math.pow(a.x,2) + Math.pow(a.y,2));
-        double sq2 = Math.sqrt(Math.pow(b.x,2) + Math.pow(b.y,2));
-        if(sq1<sq2)
-        {
-            return -1;
-        }
-        else if(sq1>sq2)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-}
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Axis[] axiss = new Axis[points.length];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[0]*a[0]+a[1]*a[1],b[0]*b[0]+b[1]*b[1]));
         for(int i=0;i<points.length;i++)
         {
-            axiss[i] = new Axis(points[i][0],points[i][1]);
+            pq.add(points[i]);
         }
-        Arrays.sort(axiss, new sortByDistance());
         
         int[][] ans = new int[k][2];
         for(int i=0;i<k;i++)
         {
-            ans[i][0]=axiss[i].x;
-            ans[i][1]=axiss[i].y;
+            int[] arr = pq.poll();
+            ans[i][0] = arr[0];
+            ans[i][1] = arr[1];
         }
         return ans;
     }
