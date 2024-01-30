209882e026308996f663e127a86ef825df05f42b
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2024-01-30 21:22:05 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (42.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,11 @@
 class Solution {
     public int fib(int n) {
+        /*
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
        return memo(n,dp);
+       */
+        return Tabulation(n);
     }
     private int memo(int n, int[] dp)
     {
@@ -13,5 +16,19 @@ class Solution {
             return dp[n];
         }
         return dp[n] = memo(n-1,dp)+memo(n-2,dp);
+    }
+    private int Tabulation(int n)
+    {
+        if(n<=1)
+            return n;
+        
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
