9c3e06e79105acb559dce534a56be9a887c5912a
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2024-01-30 14:54:25 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (25.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,8 +1,11 @@
 class Solution {
     public int rob(int[] nums) {
+        /*
         int[] dp = new int[nums.length+1];
         Arrays.fill(dp,-1);
         return Memo(nums,nums.length-1,dp);
+        */
+        return Tabulation(nums);
     }
     private int Memo(int[] arr, int idx, int[] dp)
     {
@@ -15,5 +18,21 @@ class Solution {
         int take = arr[idx]+Memo(arr,idx-2,dp);
         int ntake = Memo(arr,idx-1,dp);
         return dp[idx]=Math.max(take,ntake);
+    }
+    private int Tabulation(int[] arr)
+    {
+        int[] dp = new int[arr.length+2];
+        //base
+        dp[0]=0;
+        dp[1]=0;
+        
+        for(int i=2;i<dp.length;i++)
+        {
+            int take = dp[i-2]+arr[i-2];
+            int notTake = dp[i-1];
+            dp[i] = Math.max(take,notTake);
+        }
+        
+        return dp[dp.length-1];
     }
 }
