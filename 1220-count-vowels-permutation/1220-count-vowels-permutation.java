a8c512f90b4772e1dd54124babe15bf672216e3a
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2024-02-01 17:25:36 +0530

    Time: 649 ms (5.03%), Space: 143 MB (5.03%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -1,5 +1,6 @@
 class Solution {
     public int countVowelPermutation(int n) {
+        /*
         long[][] dp = new long[n+1][256+1];
         for(int i=0;i<n+1;i++)
         {
@@ -9,6 +10,8 @@ class Solution {
             }
         }
         return (int)solve(n,'#',dp);
+        */
+        return (int)solve2(n);
     }
     private long solve(int n, char prev, long[][] dp)
     {
@@ -46,6 +49,52 @@ class Solution {
             us = solve(n-1,'u',dp)%1000000007;
         }
         return dp[n][prev] = (as+es+is+os+us)%1000000007;
+    }
+    private long solve2(int m)
+    {
+        long[][] dp = new long[m+1][256+1];
+        //base case
+        for(int i=0;i<dp[0].length;i++)
+        {
+            dp[0][i]=1;
+        }
+        for(int n=1;n<=m;n++)
+        {
+            for(int ch=0;ch<256;ch++)
+            {
+                char prev = (char)ch;
+                
+                long as = 0;
+                if(prev == '#' || prev == 'e' || prev == 'i' || prev == 'u')
+                {
+                    as = dp[n-1][(int)'a']%1000000007;
+                }
+                long es = 0;
+                if(prev == '#' || prev == 'a' || prev == 'i')
+                {
+                    es = dp[n-1][(int)'e']%1000000007;
+                }
+                long is = 0;
+                if(prev == '#' || prev == 'e' || prev == 'o')
+                {
+                    is = dp[n-1][(int)'i']%1000000007;
+                }
+                long os = 0;
+                if(prev == '#' || prev == 'i')
+                {
+                    os = dp[n-1][(int)'o']%1000000007;
+                }
+                long us = 0;
+                if(prev == '#' || prev == 'i' || prev == 'o')
+                {
+                    us = dp[n-1][(int)'u']%1000000007;
+                }
+                dp[n][(int)prev] = (as+es+is+os+us)%1000000007;
+            }
+        }
+        
+        return dp[m][(int)'#'];
+        
     }
 }
 /*
