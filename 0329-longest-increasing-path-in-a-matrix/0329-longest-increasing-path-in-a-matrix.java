ede4815e1585a46435c621adb01aace6246e33be
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2024-02-01 16:01:35 +0530

    Time: 7 ms (96.15%), Space: 45.2 MB (25.92%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,14 +1,6 @@
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
         int ans=0;
-        int maxi=-1;
-        for(int i=0;i<matrix.length;i++)
-        {
-            for(int j=0;j<matrix[0].length;j++)
-            {
-                maxi=Math.max(maxi,matrix[i][j]);
-            }
-        }
         int[][] dp = new int[matrix.length][matrix[0].length];
         for(int i=0;i<matrix.length;i++)
         {
@@ -21,13 +13,13 @@ class Solution {
         {
             for(int j=0;j<matrix[0].length;j++)
             {
-                int path = solve(matrix,i,j,-1,dp);
+                int path = solve(matrix,i,j,dp);
                 ans=Math.max(ans,path);
             }
         }
         return ans+1;
     }
-    private int solve(int[][] matrix, int row, int col, int prev, int[][] dp)
+    private int solve(int[][] matrix, int row, int col, int[][] dp)
     {
         if(dp[row][col] !=-1)
         {
@@ -37,25 +29,25 @@ class Solution {
         int left = 0;
         if(col-1>=0 && matrix[row][col-1]>matrix[row][col])
         {
-            left = 1 + solve(matrix,row,col-1,matrix[row][col],dp);
+            left = 1 + solve(matrix,row,col-1,dp);
         }
         //right
         int right = 0;
         if(col+1<matrix[0].length && matrix[row][col+1]>matrix[row][col])
         {
-            right = 1+solve(matrix,row,col+1,matrix[row][col],dp);
+            right = 1+solve(matrix,row,col+1,dp);
         }
         //up
         int up=0;
         if(row-1>=0 && matrix[row-1][col]>matrix[row][col])
         {
-            up=1+solve(matrix,row-1,col,matrix[row][col],dp);
+            up=1+solve(matrix,row-1,col,dp);
         }
         //down
         int down=0;
         if(row+1<matrix.length && matrix[row+1][col]>matrix[row][col])
         {
-            down = 1+solve(matrix,row+1,col,matrix[row][col],dp);
+            down = 1+solve(matrix,row+1,col,dp);
         }
         return dp[row][col] = Math.max(left,Math.max(right,Math.max(up,down)));
     }
