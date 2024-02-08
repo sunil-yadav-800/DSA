6fa14193c24322278a0fb1d8e917319369e7696f
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2024-02-08 21:37:58 +0530

    Time: 8 ms (38.75%), Space: 44.7 MB (51.86%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minDistance(String word1, String word2) {
+        /*
         int[][] dp = new int[word1.length()+1][word2.length()+1];
         for(int i=0;i<dp.length;i++)
         {
@@ -9,6 +10,8 @@ class Solution {
             }
         }
         return solve(word1,word2,word1.length()-1,word2.length()-1,dp);
+        */
+        return solve2(word1,word2);
     }
     private int solve(String word1, String word2, int idx1, int idx2, int[][] dp)
     {
@@ -34,5 +37,37 @@ class Solution {
             int option2 = 1+solve(word1,word2,idx1,idx2-1,dp);
             return dp[idx1][idx2] = Math.min(option1,option2);
         }
+    }
+    private int solve2(String word1, String word2)
+    {
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
+        //base case
+        for(int i=0;i<dp[0].length;i++)
+        {
+            dp[0][i]=i;
+        }
+        for(int i=0;i<dp.length;i++)
+        {
+            dp[i][0]=i;
+        }
+        
+        for(int idx1=1;idx1<dp.length;idx1++)
+        {
+            for(int idx2=1;idx2<dp[0].length;idx2++)
+            {
+                if(word1.charAt(idx1-1) == word2.charAt(idx2-1))
+                {
+                    dp[idx1][idx2] = 0+dp[idx1-1][idx2-1];
+                }
+                else
+                {
+                    int option1 = 1+dp[idx1-1][idx2];
+                    int option2 = 1+dp[idx1][idx2-1];
+                    dp[idx1][idx2] = Math.min(option1,option2);
+                }
+            }
+        }
+        
+        return dp[word1.length()][word2.length()];
     }
 }
