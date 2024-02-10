177286996ddb44f015f2b5d5f64a6010e5c181d4
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2024-02-10 21:01:07 +0530

    Time: 1 ms (46.94%), Space: 40.2 MB (67.31%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -1,8 +1,11 @@
 class Solution {
     public int numTilings(int n) {
+        /*
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
         return solve(n,dp);
+        */
+        return solve2(n);
     }
     public int solve(int n, int[] dp)
     {
@@ -13,5 +16,22 @@ class Solution {
         if(dp[n]!=-1)
             return dp[n]%1000000007;
         return dp[n] = ((solve(n-1,dp)*2)%1000000007 + solve(n-3,dp)%1000000007)%1000000007;
+    }
+    private int solve2(int n)
+    {
+        if(n<=2)
+            return n;
+        if(n==3)
+            return 5;
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=5;
+        for(int i=4;i<=n;i++)
+        {
+            dp[i]=((dp[i-1]*2)%1000000007 + dp[i-3]%1000000007)%1000000007;
+        }
+        return dp[n];
     }
 }
