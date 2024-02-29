64a544408c893e8ca045b16b6e2ea249a38104f0
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2024-02-29 15:18:44 +0530

    Time: 15 ms (8.41%), Space: 57.6 MB (5.25%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -4,35 +4,49 @@ class Solution {
         {
             return false;
         }
-        /*
-        boolean[][][] dp = new boolean[s1.length()+1][s2.length()+1][s3.length()+1];
+        
+        int[][][] dp = new int[s1.length()+1][s2.length()+1][s3.length()+1];
+        for(int i=0;i<s1.length()+1;i++)
+        {
+            for(int j=0;j<s2.length()+1;j++)
+            {
+                for(int k=0;k<s3.length()+1;k++)
+                {
+                    dp[i][j][k]=-1;
+                }
+            }
+        }
         return solve(s1,s2,s3,0,0,0,dp);
-        */
-        return solve2(s1,s2,s3);
+        //return solve2(s1,s2,s3);
     }
-    private boolean solve(String s1, String s2, String s3, int i, int j, int k, boolean[][][] dp)
+    private boolean solve(String s1, String s2, String s3, int i, int j, int k, int[][][] dp)
     {
         if(k == s3.length())
         {
             return true;
         }
-        if(dp[i][j][k])
+        if(dp[i][j][k]!=-1)
         {
-            return dp[i][j][k];
+            return dp[i][j][k]==1?true:false;
         }
         if(i<s1.length() && j<s2.length() && s1.charAt(i) == s3.charAt(k) && s2.charAt(j) == s3.charAt(k))
         {
             boolean takeFromS1 = solve(s1,s2,s3,i+1,j,k+1,dp);
             boolean takeFromS2 = solve(s1,s2,s3,i,j+1,k+1,dp);
-            return dp[i][j][k] = (takeFromS1 || takeFromS2);
+            dp[i][j][k]=(takeFromS1 || takeFromS2) == true ? 1:0;
+            return (takeFromS1 || takeFromS2);
         }
         else if(i<s1.length() && s1.charAt(i) == s3.charAt(k))
         {
-            return dp[i][j][k] = solve(s1,s2,s3,i+1,j,k+1,dp);
+            boolean result = solve(s1,s2,s3,i+1,j,k+1,dp);
+            dp[i][j][k] = result==true?1:0;
+            return result;
         }
         else if(j<s2.length() && s2.charAt(j) == s3.charAt(k))
         {
-            return dp[i][j][k] = solve(s1,s2,s3,i,j+1,k+1,dp);
+            boolean result = solve(s1,s2,s3,i,j+1,k+1,dp);
+            dp[i][j][k] = result==true?1:0;
+            return result;
         }
         return false; //if not above case
     }
