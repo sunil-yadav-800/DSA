97b1e2f6e6de71508fde97c51052b5c5a7fbe61f
sunil-yadav-800 <101914081+sunil-yadav-800@users.noreply.github.com>
2024-03-11 21:49:36 +0530

    Time: 115 ms (28.84%), Space: 63.8 MB (7.83%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,27 +1,30 @@
 class Solution {
     public int countPrimes(int n) {
-        int[] seive = new int[n];
-        for(int i=2;i<n;i++)
+        if(n==0 || n==1)
         {
-            seive[i]=1;
+            return 0;
         }
+        int[] sieve = new int[n];
+        Arrays.fill(sieve,1);
+        sieve[0]=0;
+        sieve[1]=0;
         for(int i=2;i*i<n;i++)
         {
-            if(seive[i]==1)
+            if(sieve[i]==1)
             {
-            for(int j=i*i;j<n;j+=i)
-            {
-                seive[j]=0;
+                for(int j=i*i;j<n;j+=i)
+                {
+                    sieve[j]=0;
+                }
             }
-            }
-        } 
-        
-        int cnt=0;
-        for(int i=2;i<n;i++)
-        {
-            if(seive[i]==1)
-                cnt++;
         }
-        return cnt;
+        
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(sieve[i]==1)
+                count++;
+        }
+        return count;
     }
 }
